b38be6868e30c4d8417d26fdb29caef1ca85bcbd
livedave04 <contactodavidvas1@gmail.com>
2024-09-15 13:10:41 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+const TOKEN = '6808586807:AAH5KvJxwCHbxVecJJ1D2fQyiVCK4u_RuBQ'; // Reemplaza con el token de tu bot de Telegram
+const CHAT_ID = '5157616506'; // Reemplaza con el chat ID donde se enviarán los mensajes
